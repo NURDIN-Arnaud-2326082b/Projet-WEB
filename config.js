9e66be4b162e480e7685ebcaa9280ec598df13ae
Arnaud Nurdin <arnaud.nurdin@etu.univ-amu.fr>
2024-01-11 11:58:45 +0100

    btn

---
+++ config.js
@@ -0,0 +1,20 @@
+button.addEventListener("click", () => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+  
+    // update the button text
+    const newCta = newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
+    button.innerText = newCta;  
+  
+    // use an aria-label if you are omitting text on the button
+    // and using sun/moon icons, for example
+    button.setAttribute("aria-label", newCta);
+  
+    // update theme attribute on HTML to switch theme in CSS
+    document.querySelector("html").setAttribute("data-theme", newTheme);
+  
+    // update in local storage
+    localStorage.setItem("theme", newTheme);
+  
+    // update the currentThemeSetting in memory
+    currentThemeSetting = newTheme;
+  });
